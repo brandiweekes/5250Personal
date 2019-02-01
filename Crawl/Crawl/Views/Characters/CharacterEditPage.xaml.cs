a2080bd80b42be9a6d9711c35a9c6703bae4eff3
brandiweekes <brandiweekes@gmail.com>
2019-01-31 16:07:51 -0800

    character updates; NOTE attributes are not enums for character, but they are for items

--- Crawl/Crawl/Views/Characters/CharacterEditPage.xaml.cs
+++ Crawl/Crawl/Views/Characters/CharacterEditPage.xaml.cs
@@ -26,16 +26,19 @@ namespace Crawl.Views
 
             // Set the data binding for the page
             BindingContext = _viewModel = viewModel;
+
+            ////Need to make the SelectedItem a string, so it can select the correct item.
+            //AttributePicker.SelectedItem = Data.Attribute.ToString();
         }
 
 	    public async void Save_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "EditData", Data);
 
-            // removing the old ItemDetails page, 2 up counting this page
+            // removing the old CharacterDetails page, 2 up counting this page
             Navigation.RemovePage(Navigation.NavigationStack[Navigation.NavigationStack.Count - 2]);
 
-            // Add a new items details page, with the new Item data on it
+            // Add a new characters details page, with the new Character data on it
             await Navigation.PushAsync(new CharacterDetailPage(new CharacterDetailViewModel(Data)));
 
             // Last, remove this page
@@ -45,6 +48,12 @@ namespace Crawl.Views
 	    private async void Cancel_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
+        }
+
+        // The stepper function for Age
+        void Age_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            AgeValue.Text = String.Format("{0}", e.NewValue);
         }
     }
 }
